7658c559bf5d9eab1941b964b898584bb2e399d0
Akiomi Kamakura <akiomik@gmail.com>
2024-01-24 08:04:03 +0900

    Improve index.js

--- index.js
+++ index.js
@@ -1,52 +1,20 @@
 import('./pkg')
   .then(rust_module => {
-    const c_button = document.getElementById("c");
-    c_button.addEventListener("click", async () => {
-      const async_player = new rust_module.Player(rust_module.Note.C3);
-      player = await async_player;
-      player.start();
-    });
-
-    const d_button = document.getElementById("d");
-    d_button.addEventListener("click", async () => {
-      const async_player = new rust_module.Player(rust_module.Note.D3);
-      player = await async_player;
-      player.start();
-    });
-
-    const e_button = document.getElementById("e");
-    e_button.addEventListener("click", async () => {
-      const async_player = new rust_module.Player(rust_module.Note.E3);
-      player = await async_player;
-      player.start();
-    });
-
-    const f_button = document.getElementById("f");
-    f_button.addEventListener("click", async () => {
-      const async_player = new rust_module.Player(rust_module.Note.F3);
-      player = await async_player;
-      player.start();
-    });
-
-    const g_button = document.getElementById("g");
-    g_button.addEventListener("click", async () => {
-      const async_player = new rust_module.Player(rust_module.Note.G3);
-      player = await async_player;
-      player.start();
-    });
-
-    const a_button = document.getElementById("a");
-    a_button.addEventListener("click", async () => {
-      const async_player = new rust_module.Player(rust_module.Note.A3);
-      player = await async_player;
-      player.start();
-    });
-
-    const b_button = document.getElementById("b");
-    b_button.addEventListener("click", async () => {
-      const async_player = new rust_module.Player(rust_module.Note.B3);
-      player = await async_player;
-      player.start();
+    [
+      [rust_module.Note.C3, "c"],
+      [rust_module.Note.D3, "d"],
+      [rust_module.Note.E3, "e"],
+      [rust_module.Note.F3, "f"],
+      [rust_module.Note.G3, "g"],
+      [rust_module.Note.A3, "a"],
+      [rust_module.Note.B3, "b"],
+    ].forEach(([note, id]) => {
+      const button = document.getElementById(id);
+      button.addEventListener("click", async () => {
+        const async_player = new rust_module.Player(note);
+        player = await async_player;
+        player.start();
+      });
     });
   })
   .catch(console.error);
