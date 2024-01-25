4a2df08a1bfdd51ab884ddedbdbdebdf1a42013a
Akiomi Kamakura <akiomik@gmail.com>
2024-01-25 23:39:45 +0900

    Improve Note

--- src/note.rs
+++ src/note.rs
@@ -31,61 +31,61 @@ pub enum Note {
 
 impl Note {
     pub fn octave(&self) -> u8 {
+        self.note_number() / 12 - 1
+    }
+
+    pub fn note_number(&self) -> u8 {
         match self {
-            Note::C3 => 3,
-            Note::Csharp3 => 3,
-            Note::D3 => 3,
-            Note::Dsharp3 => 3,
-            Note::E3 => 3,
-            Note::F3 => 3,
-            Note::Fsharp3 => 3,
-            Note::G3 => 3,
-            Note::Gsharp3 => 3,
-            Note::A3 => 3,
-            Note::Asharp3 => 3,
-            Note::B3 => 3,
-            Note::C4 => 4,
-            Note::Csharp4 => 4,
-            Note::D4 => 4,
-            Note::Dsharp4 => 4,
-            Note::E4 => 4,
-            Note::F4 => 4,
-            Note::Fsharp4 => 4,
-            Note::G4 => 4,
-            Note::Gsharp4 => 4,
-            Note::A4 => 4,
-            Note::Asharp4 => 4,
-            Note::B4 => 4,
+            Note::C3 => 48,
+            Note::Csharp3 => 49,
+            Note::D3 => 50,
+            Note::Dsharp3 => 51,
+            Note::E3 => 52,
+            Note::F3 => 53,
+            Note::Fsharp3 => 54,
+            Note::G3 => 55,
+            Note::Gsharp3 => 56,
+            Note::A3 => 57,
+            Note::Asharp3 => 58,
+            Note::B3 => 59,
+            Note::C4 => 60,
+            Note::Csharp4 => 61,
+            Note::D4 => 62,
+            Note::Dsharp4 => 63,
+            Note::E4 => 64,
+            Note::F4 => 65,
+            Note::Fsharp4 => 66,
+            Note::G4 => 67,
+            Note::Gsharp4 => 68,
+            Note::A4 => 69,
+            Note::Asharp4 => 70,
+            Note::B4 => 71,
         }
     }
 
     // TODO: support other base frequencies (e.g. 442, 444)
     pub fn freq(&self) -> f32 {
-        match self {
-            Note::C3 => 130.81,
-            Note::Csharp3 => 138.59,
-            Note::D3 => 146.83,
-            Note::Dsharp3 => 155.56,
-            Note::E3 => 164.81,
-            Note::F3 => 174.61,
-            Note::Fsharp3 => 185.00,
-            Note::G3 => 196.00,
-            Note::Gsharp3 => 207.65,
-            Note::A3 => 220.00,
-            Note::Asharp3 => 233.08,
-            Note::B3 => 246.94,
-            Note::C4 => 261.63,
-            Note::Csharp4 => 277.18,
-            Note::D4 => 293.66,
-            Note::Dsharp4 => 311.13,
-            Note::E4 => 329.63,
-            Note::F4 => 349.23,
-            Note::Fsharp4 => 369.99,
-            Note::G4 => 392.00,
-            Note::Gsharp4 => 415.30,
-            Note::A4 => 440.00,
-            Note::Asharp4 => 466.16,
-            Note::B4 => 493.88,
-        }
+        440.0 * (2.0 as f32).powf((self.note_number() as i8 - 69) as f32 / 12.0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_octave() {
+        assert_eq!(Note::C3.octave(), 3);
+        assert_eq!(Note::A3.octave(), 3);
+        assert_eq!(Note::C4.octave(), 4);
+        assert_eq!(Note::A4.octave(), 4);
+    }
+
+    #[test]
+    fn test_freq() {
+        assert_eq!(Note::C3.freq(), 130.81277);
+        assert_eq!(Note::A3.freq(), 220.0);
+        assert_eq!(Note::C4.freq(), 261.62555);
+        assert_eq!(Note::A4.freq(), 440.0);
     }
 }
