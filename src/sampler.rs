873737459f3c1da88ad4b2c544e92689fca29006
Akiomi Kamakura <akiomik@gmail.com>
2024-01-27 07:20:19 +0900

    Add tests for Sampler#find_closest_note_in_samples

--- src/sampler.rs
+++ src/sampler.rs
@@ -36,9 +36,14 @@ impl Sampler {
         Ok(AudioBuffer::from(decoded))
     }
 
-    fn find_closest_note_in_samples(&self, note: &Note) -> Note {
+    fn find_closest_note_in_samples(&self, note: &Note) -> Option<Note> {
+        if self.samples.is_empty() {
+            return None;
+        }
+
         let note_number = note.note_number() as i16;
-        self.samples
+        let closest_note = self
+            .samples
             .keys()
             .fold((Note::A4, i16::MAX), |(acc_key, acc_diff), key| {
                 let diff = (note_number - key.note_number() as i16).abs();
@@ -48,16 +53,18 @@ impl Sampler {
                     (acc_key, acc_diff)
                 }
             })
-            .0
+            .0;
+        Some(closest_note)
     }
 
     fn calc_note_and_playback_rate(&self, note: &Note) -> (Note, f32) {
         if self.samples.contains_key(note) {
             (note.clone(), 1.0)
         } else {
-            let closest_note = self.find_closest_note_in_samples(note);
+            let closest_note = self
+                .find_closest_note_in_samples(note)
+                .expect("closest note should be found"); // TODO
             let closest_freq = closest_note.freq();
-            let closest_note = self.find_closest_note_in_samples(note);
             (closest_note, note.freq() / closest_freq)
         }
     }
@@ -72,5 +79,87 @@ impl Sampler {
         let src = AudioBufferSourceNode::new_with_options(&self.ctx, &opts)?;
         src.set_buffer(Some(buffer));
         Ok(src)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use wasm_bindgen_test::*;
+
+    wasm_bindgen_test_configure!(run_in_browser);
+
+    use super::*;
+
+    const A2: &[u8] = include_bytes!("../samples/a2.m4a").as_slice();
+    const A3: &[u8] = include_bytes!("../samples/a3.m4a").as_slice();
+
+    #[wasm_bindgen_test]
+    async fn test_find_closest_note_in_samples_0() {
+        let ctx = AudioContext::new().unwrap();
+        let samples = HashMap::new();
+        let sampler = Sampler::new(ctx, samples).await.unwrap();
+        assert_eq!(sampler.find_closest_note_in_samples(&Note::A2), None);
+    }
+
+    #[wasm_bindgen_test]
+    async fn test_find_closest_note_in_samples_1_contains() {
+        let ctx = AudioContext::new().unwrap();
+        let mut samples = HashMap::new();
+        samples.insert(Note::A2, A2.into());
+        let sampler = Sampler::new(ctx, samples).await.unwrap();
+        assert_eq!(
+            sampler.find_closest_note_in_samples(&Note::A2),
+            Some(Note::A2)
+        );
+    }
+
+    #[wasm_bindgen_test]
+    async fn test_find_closest_note_in_samples_1_not_contains() {
+        let ctx = AudioContext::new().unwrap();
+        let mut samples = HashMap::new();
+        samples.insert(Note::A2, A2.into());
+        let sampler = Sampler::new(ctx, samples).await.unwrap();
+        assert_eq!(
+            sampler.find_closest_note_in_samples(&Note::C2),
+            Some(Note::A2)
+        );
+    }
+
+    #[wasm_bindgen_test]
+    async fn test_find_closest_note_in_samples_2_contains() {
+        let ctx = AudioContext::new().unwrap();
+        let mut samples = HashMap::new();
+        samples.insert(Note::A2, A2.into());
+        samples.insert(Note::A3, A3.into());
+        let sampler = Sampler::new(ctx, samples).await.unwrap();
+        assert_eq!(
+            sampler.find_closest_note_in_samples(&Note::A2),
+            Some(Note::A2)
+        );
+        assert_eq!(
+            sampler.find_closest_note_in_samples(&Note::A3),
+            Some(Note::A3)
+        );
+    }
+
+    #[wasm_bindgen_test]
+    async fn test_find_closest_note_in_samples_2_not_contains() {
+        let ctx = AudioContext::new().unwrap();
+        let mut samples = HashMap::new();
+        samples.insert(Note::A2, A2.into());
+        samples.insert(Note::A3, A3.into());
+        let sampler = Sampler::new(ctx, samples).await.unwrap();
+        assert_eq!(
+            sampler.find_closest_note_in_samples(&Note::C2),
+            Some(Note::A2)
+        );
+        assert_eq!(
+            sampler.find_closest_note_in_samples(&Note::C3),
+            Some(Note::A2)
+        );
+        assert_eq!(
+            sampler.find_closest_note_in_samples(&Note::C4),
+            Some(Note::A3)
+        );
     }
 }
