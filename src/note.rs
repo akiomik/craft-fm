ebc7867a05e8f5565d90ab8de07bf364f96b7508
Akiomi Kamakura <akiomik@gmail.com>
2024-01-29 18:54:01 +0900

    Add notes between C1 and B1

--- src/note.rs
+++ src/note.rs
@@ -5,6 +5,18 @@ use wasm_bindgen::prelude::*;
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[wasm_bindgen]
 pub enum Note {
+    C1,
+    Csharp1,
+    D1,
+    Dsharp1,
+    E1,
+    F1,
+    Fsharp1,
+    G1,
+    Gsharp1,
+    A1,
+    Asharp1,
+    B1,
     C2,
     Csharp2,
     D2,
@@ -48,6 +60,18 @@ impl Note {
 
     pub fn from_note_number(note_number: u8) -> Option<Self> {
         match note_number {
+            24 => Some(Note::C1),
+            25 => Some(Note::Csharp1),
+            26 => Some(Note::D1),
+            27 => Some(Note::Dsharp1),
+            28 => Some(Note::E1),
+            29 => Some(Note::F1),
+            30 => Some(Note::Fsharp1),
+            31 => Some(Note::G1),
+            32 => Some(Note::Gsharp1),
+            33 => Some(Note::A1),
+            34 => Some(Note::Asharp1),
+            35 => Some(Note::B1),
             36 => Some(Note::C2),
             37 => Some(Note::Csharp2),
             38 => Some(Note::D2),
@@ -102,6 +126,18 @@ impl Note {
 
     pub fn note_number(&self) -> u8 {
         match self {
+            Note::C1 => 24,
+            Note::Csharp1 => 25,
+            Note::D1 => 26,
+            Note::Dsharp1 => 27,
+            Note::E1 => 28,
+            Note::F1 => 29,
+            Note::Fsharp1 => 30,
+            Note::G1 => 31,
+            Note::Gsharp1 => 32,
+            Note::A1 => 33,
+            Note::Asharp1 => 34,
+            Note::B1 => 35,
             Note::C2 => 36,
             Note::Csharp2 => 37,
             Note::D2 => 38,
@@ -184,6 +220,8 @@ mod tests {
 
     #[test]
     fn test_octave() {
+        assert_eq!(Note::C1.octave(), 1);
+        assert_eq!(Note::A1.octave(), 1);
         assert_eq!(Note::C2.octave(), 2);
         assert_eq!(Note::A2.octave(), 2);
         assert_eq!(Note::C3.octave(), 3);
@@ -194,6 +232,8 @@ mod tests {
 
     #[test]
     fn test_octave_up() {
+        assert_eq!(Note::C1.octave_up(), Some(Note::C2));
+        assert_eq!(Note::A1.octave_up(), Some(Note::A2));
         assert_eq!(Note::C2.octave_up(), Some(Note::C3));
         assert_eq!(Note::A2.octave_up(), Some(Note::A3));
         assert_eq!(Note::C3.octave_up(), Some(Note::C4));
@@ -204,8 +244,10 @@ mod tests {
 
     #[test]
     fn test_octave_down() {
-        assert_eq!(Note::C2.octave_down(), None);
-        assert_eq!(Note::A2.octave_down(), None);
+        assert_eq!(Note::C1.octave_down(), None);
+        assert_eq!(Note::A1.octave_down(), None);
+        assert_eq!(Note::C2.octave_down(), Some(Note::C1));
+        assert_eq!(Note::A2.octave_down(), Some(Note::A1));
         assert_eq!(Note::C3.octave_down(), Some(Note::C2));
         assert_eq!(Note::A3.octave_down(), Some(Note::A2));
         assert_eq!(Note::C4.octave_down(), Some(Note::C3));
@@ -214,6 +256,8 @@ mod tests {
 
     #[test]
     fn test_freq() {
+        assert_eq!(Note::C1.freq(), 32.703194);
+        assert_eq!(Note::A1.freq(), 55.0);
         assert_eq!(Note::C2.freq(), 65.40639);
         assert_eq!(Note::A2.freq(), 110.0);
         assert_eq!(Note::C3.freq(), 130.81277);
@@ -224,6 +268,8 @@ mod tests {
 
     #[test]
     fn test_pitch_class() {
+        assert_eq!(Note::C1.pitch_class(), 0);
+        assert_eq!(Note::A1.pitch_class(), 9);
         assert_eq!(Note::C2.pitch_class(), 0);
         assert_eq!(Note::A2.pitch_class(), 9);
         assert_eq!(Note::C3.pitch_class(), 0);
@@ -234,6 +280,8 @@ mod tests {
 
     #[test]
     fn test_pitch_class_label() {
+        assert_eq!(Note::C1.pitch_class_label(), "C");
+        assert_eq!(Note::A1.pitch_class_label(), "A");
         assert_eq!(Note::C2.pitch_class_label(), "C");
         assert_eq!(Note::A2.pitch_class_label(), "A");
         assert_eq!(Note::C3.pitch_class_label(), "C");
@@ -244,6 +292,8 @@ mod tests {
 
     #[test]
     fn test_format() {
+        assert_eq!(Note::C1.to_string(), "C1");
+        assert_eq!(Note::A1.to_string(), "A1");
         assert_eq!(Note::C2.to_string(), "C2");
         assert_eq!(Note::A2.to_string(), "A2");
         assert_eq!(Note::C3.to_string(), "C3");
