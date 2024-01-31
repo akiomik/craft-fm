162b79160cf9b14d0a08302b12db010e327efd2c
Akiomi Kamakura <akiomik@gmail.com>
2024-01-31 20:24:03 +0900

    Improve log signature

--- src/log.rs
+++ src/log.rs
@@ -2,6 +2,6 @@ use wasm_bindgen::JsValue;
 use web_sys::console;
 
 #[allow(dead_code)]
-pub fn log(s: String) {
-    console::log_1(&JsValue::from(s));
+pub fn log<S: Into<String>>(s: S) {
+    console::log_1(&JsValue::from(s.into()));
 }
