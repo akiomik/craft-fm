61640e888cf0015cf18252d3e37bca1ae79b2d35
Akiomi Kamakura <akiomik@gmail.com>
2024-02-06 00:27:04 +0900

    Add PitchEnvelope to Synthesizer

--- src/synthesizer.rs
+++ src/synthesizer.rs
@@ -1,25 +1,47 @@
 use web_sys::{AudioContext, GainNode, OscillatorType};
 
-use crate::{envs::AmpEnvelope, result::Result, theory::Note};
+use crate::{
+    envs::{AmpEnvelope, PitchEnvelope},
+    result::Result,
+    theory::Note,
+};
 
 #[derive(Debug, Clone)]
 #[allow(dead_code)]
 pub struct Synthesizer {
     ctx: AudioContext,
-    amp: AmpEnvelope,
     shape: OscillatorType,
+    amp: AmpEnvelope,
+    pitch: Option<PitchEnvelope>,
 }
 
 #[allow(dead_code)]
 impl Synthesizer {
-    pub fn new(ctx: AudioContext, amp: AmpEnvelope, shape: OscillatorType) -> Self {
-        Self { ctx, amp, shape }
+    pub fn new(
+        ctx: AudioContext,
+        shape: OscillatorType,
+        amp: AmpEnvelope,
+        pitch: Option<PitchEnvelope>,
+    ) -> Self {
+        Self {
+            ctx,
+            shape,
+            amp,
+            pitch,
+        }
     }
 
     pub fn node(&self, note: &Note, time: f64, duration: f64) -> Result<GainNode> {
-        let osc = self.ctx.create_oscillator()?;
+        let mut osc = self.ctx.create_oscillator()?;
         osc.set_type(self.shape);
-        osc.frequency().set_value(note.freq().into());
+
+        if let Some(ref pitch) = self.pitch {
+            // TODO: note support
+            pitch.attach(&mut osc, time, duration)?;
+        } else {
+            osc.frequency().set_value(note.freq().into());
+        }
+
         osc.start_with_when(time)?;
         osc.stop_with_when(time + duration + self.amp.release())?;
 
