c24287c42d4ffaeede068a345c9870a6af35c8b0
Akiomi Kamakura <akiomik@gmail.com>
2024-02-05 22:05:14 +0900

    Add Arpeggiator type

--- src/arps.rs
+++ src/arps.rs
@@ -7,3 +7,8 @@ pub use down::DownArpeggiator;
 #[allow(unused_imports)]
 pub use up::UpArpeggiator;
 pub use up_down::UpDownArpeggiator;
+
+use crate::theory::Note;
+
+#[allow(dead_code)]
+pub type Arpeggiator = dyn Iterator<Item = Note>;
