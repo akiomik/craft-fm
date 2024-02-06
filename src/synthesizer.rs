191d0252e19a4274892c16215c910e44f3a650e1
Akiomi Kamakura <akiomik@gmail.com>
2024-02-06 10:21:48 +0900

    Change Synthesizer#node to node_with_note and node_with_pitch_envelope

--- src/synthesizer.rs
+++ src/synthesizer.rs
@@ -12,35 +12,37 @@ pub struct Synthesizer {
     ctx: AudioContext,
     shape: OscillatorType,
     amp: AmpEnvelope,
-    pitch: Option<PitchEnvelope>,
 }
 
 #[allow(dead_code)]
 impl Synthesizer {
-    pub fn new(
-        ctx: AudioContext,
-        shape: OscillatorType,
-        amp: AmpEnvelope,
-        pitch: Option<PitchEnvelope>,
-    ) -> Self {
-        Self {
-            ctx,
-            shape,
-            amp,
-            pitch,
-        }
+    pub fn new(ctx: AudioContext, shape: OscillatorType, amp: AmpEnvelope) -> Self {
+        Self { ctx, shape, amp }
     }
 
-    pub fn node(&self, note: &Note, time: f64, duration: f64) -> Result<GainNode> {
+    pub fn node_with_note(&self, note: &Note, time: f64, duration: f64) -> Result<GainNode> {
+        let osc = self.ctx.create_oscillator()?;
+        osc.set_type(self.shape);
+
+        osc.frequency().set_value(note.freq().into());
+
+        osc.start_with_when(time)?;
+        osc.stop_with_when(time + duration + self.amp.release())?;
+
+        let gain = self.amp.node(&osc, time, duration)?;
+        Ok(gain)
+    }
+
+    pub fn node_with_pitch_envelope(
+        &self,
+        envelope: PitchEnvelope,
+        time: f64,
+        duration: f64,
+    ) -> Result<GainNode> {
         let mut osc = self.ctx.create_oscillator()?;
         osc.set_type(self.shape);
 
-        if let Some(ref pitch) = self.pitch {
-            // TODO: note support
-            pitch.attach(&mut osc, time, duration)?;
-        } else {
-            osc.frequency().set_value(note.freq().into());
-        }
+        envelope.attach(&mut osc, time, duration)?;
 
         osc.start_with_when(time)?;
         osc.stop_with_when(time + duration + self.amp.release())?;
